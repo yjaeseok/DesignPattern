5dea8df6a6f3f5a79fad9d374a2d0cafc98bdaf8
Jake Yoon <yjaeseok@gmail.com>
2018-04-27 07:19:16 +0900

    Chapter 7. Builder Pattern 2

--- BuilderPattern/src/Main.java
+++ BuilderPattern/src/Main.java
@@ -1,4 +1,5 @@
 import lib.Computer;
+import lib.ComputerBuilder;
 import lib.ComputerFactory;
 import lib.LGGramBluePrint;
 
@@ -8,6 +9,7 @@ public class Main {
     // 복잡한 단계가 있는 인스턴스 생성을 단순화
     public static void main(String[] args) {
 
+        // Builder Pattern 1
         ComputerFactory factory = new ComputerFactory();
 
         // 설계도를 가지고
@@ -18,5 +20,16 @@ public class Main {
         Computer computer = factory.getComputer();
 
         System.out.println(computer);
+
+
+        // Builder Pattern2
+        Computer computer2 = ComputerBuilder
+                .start()
+                .setCpu("I8")
+                .setRam("16GB")
+                .setStorage("512GB SSD")
+                .build();
+
+        System.out.println(computer2);
     }
 }
